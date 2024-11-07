b575c9cc47d007bf647951949fcc7d30d95b0fd8
momen165 <35774864+momen165@users.noreply.github.com>
2024-11-08 00:05:57 +0200

    Remove unused CourseDetail component import from CourseDetailPage

--- website/client/src/pages/user/CourseDetailPage.js
+++ website/client/src/pages/user/CourseDetailPage.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'styles/CourseDetailPage.css';
 import Navbar from 'components/Navbar';
-import CourseDetail from 'components/CourseDetail'; // Import the new component
+
 
 const CourseDetailPage = () => {
   return (
     <div className="course-detail-page">
       <Navbar />
-      <CourseDetail /> {/* Use the new component here */}
+      
     </div>
   );
 };
