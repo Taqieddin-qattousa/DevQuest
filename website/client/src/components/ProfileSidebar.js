50a22a8217e6889e02fc90b70319da371ace0479
momen165 <35774864+momen165@users.noreply.github.com>
2024-11-10 01:07:15 +0200

    Enhance ProfileSidebar with icons for Profile, Login, and Billing links; update styles for better visual appeal

--- website/client/src/components/ProfileSidebar.js
+++ website/client/src/components/ProfileSidebar.js
@@ -1,19 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import 'styles/ProfileSidebar.css';
-
+import { FaCreditCard    } from 'react-icons/fa';
+import { FaRightToBracket,FaUser } from 'react-icons/fa6';
 function Sidebar({ activeLink }) {
+  
   return (
+    
     <div className="sidebar">
       <ul>
         <Link to="/profile" className="link">
-          <li className={activeLink === 'profile' ? 'active' : ''}>Profile</li>
+          <li className={activeLink === 'profile' ? 'active' : ''}> <FaUser size={24} color='#007BFF' style={{  marginRight: '8px'  }} />  Profile</li>
+          
         </Link>
         <Link to="/changepassword" className="link">
-          <li className={activeLink === 'login' ? 'active' : ''}>Login</li>
+  <li className={activeLink === 'login' ? 'active' : ''}> <FaRightToBracket size={24} color='#007BFF' style={{  marginRight: '8px'  }} /> Login</li>
         </Link>
         <Link to="/billing" className="link">
-          <li className={activeLink === 'billing' ? 'active' : ''}>Billing</li>
+          <li className={activeLink === 'billing' ? 'active' : ''}> <FaCreditCard size={24} color='#007BFF' style={{  marginRight: '8px'  }} /> Billing</li>
         </Link>
       </ul>
     </div>
